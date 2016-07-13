12850288f70f9dbda7a8d36249b7c7fcf3570885
zhanggangbz <254586251@qq.com>
2016-07-13 11:30:35 -0400

    首次提交，搭建了项目，弄通了上传

---
+++ ALBC_REST_SDK/ALBCClient.cs
@@ -0,0 +1,41 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALBC_REST_SDK
+{
+    public class ALBCClient
+    {
+        //不计算了，我自己之前算好的一个永不失效的TOKEN，是website空间
+        public String TOKEN_STR = "";
+        public bool UpLoadFile(String _filePath)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("http://upload.media.aliyun.com");
+
+            var request = new RestRequest("/api/proxy/upload", Method.POST);
+            request.AddHeader("Accept-Encoding", "gzip,deflate");
+            request.AddHeader("Content-Type", "multipart/form-data");
+            request.AddHeader("Authorization", TOKEN_STR);
+
+            request.AddParameter("dir", "qxqzxcom");
+            request.AddParameter("name","qxqzx.jpg");
+
+            FileStream fs1 = File.OpenRead(_filePath);
+            long filesize = fs1.Length;
+            fs1.Close();
+
+            request.AddParameter("size", filesize);
+            request.AddFile("content", _filePath, "application/octet-stream");
+
+
+            IRestResponse response = client.Execute(request);
+            return false;
+        }
+    }
+}
---
+++ SDK_Test/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SDK_Test
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //创建一个打开文件式的对话框  
+            OpenFileDialog ofd = new OpenFileDialog();
+            //设置这个对话框的起始打开路径  
+            ofd.InitialDirectory = @"D:\";
+            //设置打开的文件的类型，注意过滤器的语法  
+            ofd.Filter = "PNG图片|*.png|JPG图片|*.jpg";
+            //调用ShowDialog()方法显示该对话框，该方法的返回值代表用户是否点击了确定按钮  
+            if (ofd.ShowDialog() == true)
+            {
+                ALBC_REST_SDK.ALBCClient _client = new ALBC_REST_SDK.ALBCClient();
+                _client.UpLoadFile(ofd.FileName);
+            }
+            else
+            {
+                MessageBox.Show("没有选择图片");
+            }
+        }
+    }
+}
